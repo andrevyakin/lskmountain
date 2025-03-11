f1192dab0967df77d8c70d0c86a5dd45d75d709f
andrevyakin <andrevyakin@gmail.com>
2025-03-12 00:00:53 +0300

    hero 2xl

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -5,36 +5,54 @@ const Hero = () => {
   return (
     <>
       <div className='bg-[#222222]'>
-        <div className='h-lvh bg-[url(/bg_mountain.png)] opacity-90'>
+        <div className='h-lvh bg-[url(/bg_mountain.png)] bg-no-repeat bg-cover opacity-90 pt-36 px-16 pb-8'>
           {/*для 4-х элементов*/}
-          <div className='h-lvh mt-36 bg-red-600'>
+          <div className='h-full flex flex-col justify-evenly px-44'>
             {/*для двух верхних элементов*/}
-            <div className='flex flex-row align-middle bg-blue-800'>
+            <div className='flex justify-center'>
               {/*верхний левый элемент*/}
-              <div className='basis-1/2 m-auto bg-pink-500'>
-                <h1 className='font-prata uppercase text-5xl text-center'>
-                  щебень, щпс, отсев
-                </h1>
+              <div className='flex-1 content-center justify-items-center border border-[#F0F0F0]'>
+                <div className='flex flex-col gap-3 w-fit'>
+                  <h1 className='font-prata uppercase text-5xl text-[#F0F0F0]'>
+                    щебень, щпс, отсев
+                  </h1>
+                  <div className='flex items-center gap-2'>
+                    <p className='w-16 md:w-11 h-[1px] bg-[#BABABA]'></p>
+                    <p className='font-prata uppercase text-2xl text-[#F0F0F0]'>
+                      от производителя
+                    </p>
+                  </div>
+                </div>
               </div>
 
               {/*верхний правый элемент*/}
-              <div className='flex basis-1/2 bg-amber-400 justify-center'>
-                <img src={photo1} alt='Фото1' />
+              <div className='flex-1 border border-[#F0F0F0]'>
+                <img
+                  src={photo1}
+                  alt='Фото1'
+                  className='object-cover w-full h-full'
+                />
               </div>
             </div>
 
             {/*для двух нижних элементов*/}
-            <div className='flex flex-row'>
+            <div className='flex justify-center'>
               {/*нижний левый элемент*/}
-              <div className='basis-1/2 justify-center'>
-                <img src={photo2} alt='Фото1' />
+              <div className='flex-1 border border-[#F0F0F0]'>
+                <img
+                  src={photo2}
+                  alt=' Фото2'
+                  className='object-cover w-full h-full'
+                />
               </div>
 
               {/*нижний правый элемент*/}
-              <div className='basis-1/2 font-prata uppercase text-5xl'>
-                <p>петровеньковское</p>
-                <p>месрождение</p>
-                <p>песчаника</p>
+              <div className='flex-1 content-center justify-items-center border border-[#F0F0F0]'>
+                <div className='flex flex-col gap-3 w-fit font-prata uppercase text-5xl text-[#F0F0F0]'>
+                  <p>петровеньковское</p>
+                  <p>месторождение</p>
+                  <p>песчаника</p>
+                </div>
               </div>
             </div>
           </div>
