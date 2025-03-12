5c3ed852481eca28a9f45010bbc79c837bdd50f9
andrevyakin <andrevyakin@gmail.com>
2025-03-13 01:23:56 +0300

    menu v2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,21 +4,21 @@ import email from '../assets/mail.png';
 
 const Navbar = () => {
   return (
-    <div className='flex fixed top-0 left-0 right-0 h-32 z-10 backdrop-opacity-100'>
-      <nav className="flex w-full justify-evenly items-center ml-16 mr-16 px-28 font-prata text-3xl uppercase tracking-wider border border-b border-[#F0F0F0] text-[#F0F0F0]">
+    <div className='sticky top-0 left-0 right-0 z-10 bg-[#222222] bg-[url(/bg_mountain.png)] brightness-110'>
+      <nav className='flex justify-evenly items-center mx-16 px-28 font-prata text-3xl text-[#F0F0F0] uppercase tracking-wider border border-b border-[#F0F0F0]'>
         <img src={logo} alt='ЛСК' className='object-cover pt-6' />
         <a href='#hero'>Главная</a>
         <a href='#products'>Продукция</a>
         <a href='#'>О нас</a>
         <a href='#'>Контакты</a>
-        <div className="flex flex-col gap-1.5">
+        <div className='flex flex-col gap-1.5'>
           <div className='flex text-2xl lowercase'>
             <img src={phone} alt='' />
             <p>+7 (959) 222 22-22</p>
           </div>
           <div className='flex text-2xl lowercase'>
             <img src={email} alt='' />
-            <p className="indent-2.5">ooolsklpr@mail.ru</p>
+            <p className='indent-2.5'>ooolsklpr@mail.ru</p>
           </div>
         </div>
       </nav>
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -4,61 +4,59 @@ import photo2 from '../assets/hero-2.png';
 const Hero = () => {
   return (
     <>
-      <div id='hero' className='bg-[#222222]'>
-        <div className='h-lvh bg-[url(/bg_mountain.png)] bg-no-repeat bg-cover opacity-90 pt-32 pb-8'>
-          {/*для 4-х элементов*/}
-          <div className='h-full flex flex-col justify-evenly mx-16 px-44 border-x border-[#F0F0F0]'>
-            {/*для двух верхних элементов*/}
-            <div className='flex justify-center'>
-              {/*верхний левый элемент*/}
-              <div className='flex-1 content-center justify-items-center border border-[#F0F0F0]'>
-                <div className='flex flex-col gap-6 w-fit'>
-                  <h1 className='font-prata uppercase text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl 3xl:text-5xl text-[#F0F0F0]'>
-                    щебень, щпс, отсев
-                  </h1>
-                  <div className='flex items-center gap-2'>
-                    <p className='min-w-16 md:w-11 h-[1px] bg-[#BABABA]'></p>
-                    <p className='font-prata uppercase text-base xl:text-lg 2xl:text-xl 3xl:text-2xl text-[#F0F0F0]'>
-                      от производителя
-                    </p>
-                  </div>
-                </div>
-              </div>
-
-              {/*верхний правый элемент*/}
-              <div className='flex-1 border border-[#F0F0F0]'>
-                <img
-                  src={photo1}
-                  alt='Фото1'
-                  className='object-cover w-full h-full'
-                />
-              </div>
-            </div>
-
-            {/*для двух нижних элементов*/}
-            <div className='flex justify-center'>
-              {/*нижний левый элемент*/}
-              <div className='flex-1 border border-[#F0F0F0]'>
-                <img
-                  src={photo2}
-                  alt=' Фото2'
-                  className='object-cover w-full h-full'
-                />
-              </div>
-
-              {/*нижний правый элемент*/}
-              <div className='flex-1 content-center justify-items-center border border-[#F0F0F0]'>
-                <div className='flex flex-col gap-3 w-fit font-prata uppercase text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl 3xl:text-5xl text-[#F0F0F0]'>
-                  <p>петровеньковское</p>
-                  <p>месторождение</p>
-                  <p>песчаника</p>
+      <div id="hero" className='bg-[url(/bg_mountain.png)] bg-no-repeat bg-cover bg-[#222222]'>
+        {/*для 4-х элементов*/}
+        <div className='h-dvh flex flex-col justify-evenly mx-16 px-44 border-x border-[#F0F0F0]'>
+          {/*для двух верхних элементов*/}
+          <div className='flex justify-center'>
+            {/*верхний левый элемент*/}
+            <div className='flex-1 content-center justify-items-center border border-[#F0F0F0]'>
+              <div className='flex flex-col gap-6 w-fit'>
+                <h1 className='font-prata uppercase text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl 3xl:text-5xl text-[#F0F0F0]'>
+                  щебень, щпс, отсев
+                </h1>
+                <div className='flex items-center gap-2'>
+                  <p className='min-w-16 md:w-11 h-[1px] bg-[#BABABA]'></p>
+                  <p className='font-prata uppercase text-base xl:text-lg 2xl:text-xl 3xl:text-2xl text-[#F0F0F0]'>
+                    от производителя
+                  </p>
                 </div>
               </div>
             </div>
+
+            {/*верхний правый элемент*/}
+            <div className='flex-1 border border-[#F0F0F0]'>
+              <img
+                src={photo1}
+                alt='Фото1'
+                className='object-cover w-full h-full'
+              />
+            </div>
           </div>
-          <div className='mx-11 border-t border-[#F0F0F0]'>
-            <div className='h-8 mx-5 border-x border-[#F0F0F0]'></div>
+
+          {/*для двух нижних элементов*/}
+          <div className='flex justify-center'>
+            {/*нижний левый элемент*/}
+            <div className='flex-1 border border-[#F0F0F0]'>
+              <img
+                src={photo2}
+                alt=' Фото2'
+                className='object-cover w-full h-full'
+              />
+            </div>
+
+            {/*нижний правый элемент*/}
+            <div className='flex-1 content-center justify-items-center border border-[#F0F0F0]'>
+              <div className='flex flex-col gap-3 w-fit font-prata uppercase text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl 3xl:text-5xl text-[#F0F0F0]'>
+                <p>петровеньковское</p>
+                <p>месторождение</p>
+                <p>песчаника</p>
+              </div>
+            </div>
           </div>
+        </div>
+        <div className='mx-11 border-t border-[#F0F0F0]'>
+          <div className='h-8 mx-5 border-x border-[#F0F0F0]'></div>
         </div>
       </div>
     </>
