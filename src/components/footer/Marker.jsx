714c045fd75db289a609aae477162a36e34c5214
andrevyakin <andrevyakin@gmail.com>
2025-03-30 18:26:32 +0300

    YMapMarker custom

---
+++ src/components/footer/Marker.jsx
@@ -0,0 +1,41 @@
+import { motion } from 'framer-motion';
+
+const Marker = ({ isOpen, onClick }) => {
+  const transition = {
+    duration: 0.5,
+  };
+  return isOpen ? (
+    <motion.div
+      layout
+      transition={transition}
+      className='cursor-pointer relative -top-22 -left-19 h-18 w-38  rounded-2xl  bg-[#e63d2e] opacity-85 blur-[.5px] '
+      onClick={onClick}
+    >
+      <div className='absolute left-[50%] top-[100%] -translate-x-[50%] -translate-y-[15%] w-0 h-0; border-l-[15px] border-l-transparent border-r-[15px] border-r-transparent border-t-[15px] border-t-[#e63d2e]'></div>
+      <motion.div
+        layout
+        transition={transition}
+        className='absolute text-center left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%] font-philosopher  text-[#232323] text-base font-semibold normal-case tracking-widest'
+      >
+        Филиал "Петровский" ООО "ЛСК"
+      </motion.div>
+    </motion.div>
+  ) : (
+    <motion.div
+      layout
+      transition={transition}
+      className='cursor-pointer relative h-12 w-12 rotate-313  bg-[#e63d2e] opacity-90  -top-14 -left-7 rounded-r-[50%]  rounded-tl-[50%] blur-[.5px]  '
+      onClick={onClick}
+    >
+      >
+      <motion.div
+        layout
+        transition={transition}
+        className='absolute left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%] font-prata  text-[#232323] text-base font-black uppercase tracking-widest rotate-47'
+      >
+        лск
+      </motion.div>
+    </motion.div>
+  );
+};
+export default Marker;
--- src/components/footer/YandexMap.jsx
+++ src/components/footer/YandexMap.jsx
@@ -17,10 +17,12 @@ import {
 import HintWindow from './HintWindow.jsx';
 import { HiLocationMarker } from 'react-icons/hi';
 import { GiPositionMarker } from 'react-icons/gi';
-import { GiStoneAxe } from "react-icons/gi";
+import { GiStoneAxe } from 'react-icons/gi';
+import Marker from './Marker.jsx';
 
 const YandexMap = () => {
   const getHint = useCallback((object) => object?.properties?.hint, []);
+  const [isOpen, setIsOpen] = React.useState(false);
 
   return (
     <YMapComponentsProvider apiKey={import.meta.env.VITE_APY_KEY} lang='ru_RU'>
@@ -28,18 +30,16 @@ const YandexMap = () => {
         <YMapDefaultSchemeLayer />
         <YMapDefaultFeaturesLayer />
         <YMapFeature geometry={features.geometry} style={features.style} />
-        <YMapHint hint={getHint}>
+        {/*<YMapHint hint={getHint}>
           <HintWindow />
-        </YMapHint>
-        {/* <YMapDefaultMarker
-          {...MARKER_PROPS}
+        </YMapHint>*/}
+        {/*<YMapDefaultMarker {...MARKER_PROPS} />*/}
 
-        >
-        </YMapDefaultMarker>*/}
         <YMapMarker {...MARKER_PROPS}>
+          <Marker isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
           {/*<HiLocationMarker className='text-6xl text-[#e63d2e] relative -top-14 -left-7 opacity-90' />*/}
           {/*<GiPositionMarker className='text-6xl text-[#e63d2e] relative -top-14 -left-7 opacity-90' />*/}
-          <GiStoneAxe className='text-6xl text-[#e63d2e] relative -top-14 -left-7 opacity-90' />
+          {/*<GiStoneAxe className='text-6xl text-black bg-[#e63d2e] opacity-85 relative -top-14 -left-7 rounded-r-[50%]  rounded-tl-[50%] rotate-320  blur-[1px] ' />*/}
         </YMapMarker>
       </YMap>
     </YMapComponentsProvider>
