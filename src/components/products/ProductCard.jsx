226f965dc3a22051c16d518ee627cf20ff773311
andrevyakin <andrevyakin@mail.ru>
2025-05-12 13:55:31 +0300

    big product card

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -10,39 +10,47 @@ const ProductCard = ({
   price,
 }) => {
   return (
-    image.length && (
-      <li className='flex flex-col gap-1 pt-4 border border-dark rounded-2xl bg-white drop-shadow-[0_5px_5px_rgba(0,0,0,1)]'>
-        <header className='font-prata text-lg uppercase font-semibold tracking-wider text-center text-dark'>
-          {name}
-        </header>
-        <article className='flex m-4 gap-3'>
-          {/*- m или обрезать фото*/}
-          <div className='-my-12 -mx-6'>
-            <img
-              src={image[0]}
-              alt={name}
-              height='294'
-              width='258'
-              className='object-cover h-max-[294px] w-max-[258px] rounded-2xl'
-            />
-          </div>
-          <div className='flex flex-col justify-center font-philosopher text-xs sm:text-sm text-start normal-case text-dark'>
-            {strength && <p>Марка прочности: {strength}</p>}
-            {frost && <p>Морозостойкость: {frost}</p>}
-            {greediness && <p>Лещадность; {greediness}</p>}
-            {stateStandard && (
-              <>
-                <p className='pt-2 sm:pt-8'>Соответствует</p>
-                {stateStandard.map((item, index) => (
-                  <p key={index}>{item}</p>
-                ))}
-              </>
-            )}
-            {price && <p className=''>Цена: {price} руб./тонна с НДС</p>}
-          </div>
-        </article>
-      </li>
-    )
+    <li className='flex flex-col gap-1 pt-4 border border-dark rounded-2xl bg-white drop-shadow-[0_5px_5px_rgba(0,0,0,1)] 3xl:h-96 3xl:w-150'>
+      <header className='font-prata text-lg uppercase font-semibold tracking-wider text-center text-dark'>
+        {name}
+      </header>
+      <article className='flex m-4 gap-3'>
+        {/*- m или обрезать фото*/}
+        <div className='3xl:hidden -my-12 -mx-6'>
+          <img
+            src={image[0]}
+            alt={name}
+            height='294'
+            width='258'
+            className='object-cover h-max-[294px] w-max-[258px] rounded-2xl'
+          />
+        </div>
+        <div className='max-3xl:hidden -my-12 -mx-6'>
+          <img
+            src={image[0]}
+            alt={name}
+            height='382'
+            width='335'
+            className='object-cover h-max-[382px] w-max-[335px] rounded-2xl'
+          />
+        </div>
+        <div
+          className='flex flex-col justify-center font-philosopher text-xs sm:text-sm text-start normal-case text-dark'>
+          {strength && <p>Марка прочности: {strength}</p>}
+          {frost && <p>Морозостойкость: {frost}</p>}
+          {greediness && <p>Лещадность; {greediness}</p>}
+          {stateStandard && (
+            <>
+              <p className='pt-2 sm:pt-8'>Соответствует</p>
+              {stateStandard.map((item, index) => (
+                <p key={index}>{item}</p>
+              ))}
+            </>
+          )}
+          {price && <p className=''>Цена: {price} руб./тонна с НДС</p>}
+        </div>
+      </article>
+    </li>
   );
 };
 export default ProductCard;
